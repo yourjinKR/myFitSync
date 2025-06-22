f8074f170ae0c19c19198b7dd87d72fc32c03ea9
위건 <juhyeon2398@naver.com>
2025-06-22 12:25:43 +0900

    운동 선택

--- fitsync/src/components/routine/RoutineAdd.jsx
+++ fitsync/src/components/routine/RoutineAdd.jsx
@@ -1,10 +1,35 @@
 import React from 'react';
+import styled from 'styled-components';
 
 const RoutineAdd = () => {
+  const RoutineAddWrapper = styled.div`
+    padding:15px;
+  `;
+
+  const SearchBox = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin: 20px 0;
+    & input {
+      padding: 5px 10px;
+      border: 1px solid #ccc;
+      border-radius: 4px;
+    }
+  `;
+
+  const handleSearch = () => {
+    
+  }
+
   return (
-    <div>
-      
-    </div>
+    <RoutineAddWrapper>
+      <SearchBox>
+        <input type="text" />
+        <button onClick={handleSearch} type="button">검색</button>
+      </SearchBox>
+      <button type="button">부위 선택</button>
+    </RoutineAddWrapper>
   );
 };
 
