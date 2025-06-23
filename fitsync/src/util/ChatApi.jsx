d8b2e42cff38af9b11dc5661cbca9b1346262cfb
김정재 <buddy1997@naver.com>
2025-06-23 14:32:03 +0900

    @stomp/stompjs, sockjs-client 의존성 설치

---
+++ fitsync/src/util/ChatApi.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ChatApi = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default ChatApi;
---
+++ fitsync/src/util/UseWebSocket.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const UseWebSocket = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default UseWebSocket;
