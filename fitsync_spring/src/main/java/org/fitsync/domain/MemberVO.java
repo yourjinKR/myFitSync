8e101b19a7c9f7f11a16fe16e0e506934edf7fd8
김정재 <buddy1997@naver.com>
2025-07-02 13:26:39 +0900

    생년월일 vo추가

--- fitsync_spring/src/main/java/org/fitsync/domain/MemberVO.java
+++ fitsync_spring/src/main/java/org/fitsync/domain/MemberVO.java
@@ -3,6 +3,8 @@ package org.fitsync.domain;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import oracle.sql.DATE;
+
 import java.sql.Timestamp;
 
 @Data
@@ -11,4 +13,5 @@ import java.sql.Timestamp;
 public class MemberVO {
 	private int member_idx, member_price, gym_idx;
 	private String member_email, member_name, member_num, member_image, member_status, member_day, member_type, member_purpose, member_time, member_disease, member_activity_area, member_info, member_info_image;
+	private DATE member_birth;
 }
