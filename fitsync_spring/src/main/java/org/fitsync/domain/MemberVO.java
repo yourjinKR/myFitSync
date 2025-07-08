928d3f9a377d31bd146817fdc3d6766b359aff7d
wigeon <juhyeon2398@naver.com>
2025-07-08 16:06:21 +0900

    DATE - > Date import 변경

--- fitsync_spring/src/main/java/org/fitsync/domain/MemberVO.java
+++ fitsync_spring/src/main/java/org/fitsync/domain/MemberVO.java
@@ -3,9 +3,8 @@ package org.fitsync.domain;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import oracle.sql.DATE;
 
-import java.sql.Timestamp;
+import java.sql.Date;
 
 @Data
 @AllArgsConstructor
@@ -13,5 +12,5 @@ import java.sql.Timestamp;
 public class MemberVO {
 	private int member_idx, gym_idx;
 	private String member_email, member_name, member_num, member_image, member_status, member_day, member_type, member_purpose, member_time, member_disease, member_activity_area, member_info, member_info_image, member_intro, member_gender;
-	private DATE member_birth;
+	private Date member_birth;
 }
